2c925ba679c3e7dc9f79b3be88b6ccbef1db9218
redskyby <pashadocenko@gmail.com>
2025-07-12 12:41:13 +0300

    Progect was init.

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierPlugin from "eslint-plugin-prettier";
+import importPlugin from "eslint-plugin-import";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+    {
+        files: ["**/*.{ts,tsx}"],
+        plugins: {
+            prettier: prettierPlugin,
+            import: importPlugin,
+        },
+        rules: {
+            "prettier/prettier": "error",
+
+            "import/order": [
+                "error",
+                {
+                    groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+                    pathGroups: [
+                        {
+                            pattern: "react",
+                            group: "external",
+                            position: "before",
+                        },
+                    ],
+                    "newlines-between": "always",
+                    alphabetize: {
+                        order: "asc",
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+        },
+    },
+];
+
+export default eslintConfig;
